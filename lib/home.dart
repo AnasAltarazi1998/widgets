661c687456e7fec3c746301cace62ae68704661f
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-05-02 15:43:51 +0300

    -- bottombar -- bottomsheet

--- lib/home.dart
+++ lib/home.dart
@@ -1,56 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shimmer/flutter_shimmer.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({Key key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  FToast fToast;
-  @override
-  void initState() {
-    super.initState();
-    fToast = FToast();
-    fToast.init(context);
-  }
-
-  _showToast() {
-    Widget toast = Container(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-      color: Colors.black12,
-      height: 50,
-      width: double.infinity,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(Icons.check),
-          SizedBox(
-            width: 12.0,
-          ),
-          Text("This is a Custom Toast"),
-        ],
-      ),
-    );
-    fToast.showToast(
-      child: toast,
-      gravity: ToastGravity.SNACKBAR,
-      toastDuration: Duration(seconds: 2),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: PlayStoreShimmer(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _showToast(),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          children: [
+            IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+            Spacer(),
+            IconButton(icon: Icon(Icons.search), onPressed: () {}),
+            IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+          ],
+        ),
       ),
+      floatingActionButton: Builder(
+        builder: (ctxt) => FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              showBottomSheet(
+                  context: ctxt,
+                  builder: (ctxt) => Scaffold(
+                        appBar: AppBar(),
+                        body: Center(
+                          child: Text('data'),
+                        ),
+                      ));
+            }),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,90 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         primaryColor: Colors.indigo.shade300,
       ),
-      theme:
-          ThemeData(primaryColor: Colors.indigo, primarySwatch: Colors.indigo),
+      theme: ThemeData(
+          primaryColor: Colors.indigo,
+          primarySwatch: Colors.indigo,
+          bottomAppBarColor: Colors.indigo,
+          iconTheme: IconThemeData(color: Colors.white),
+          floatingActionButtonTheme:
+              FloatingActionButtonThemeData(backgroundColor: Colors.black)),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Standard Bottom Sheet Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorScheme: Theme.of(context).colorScheme.copyWith(
+//               primary: Color(0xFF6200EE),
+//             ),
+//       ),
+//       home: Scaffold(
+//         // Use a Builder to get a context within the Scaffold.
+//         body: Builder(
+//           builder: (ctxt) {
+//             return Center(
+//               child: TextButton(
+//                 child: Text('SHOW BOTTOM SHEET'),
+//                 onPressed: () {
+//                   showBottomSheet(
+//                     context: ctxt,
+//                     builder: (context) {
+//                       final theme = Theme.of(context);
+//                       // Using Wrap makes the bottom sheet height the height of the content.
+//                       // Otherwise, the height will be half the height of the screen.
+//                       return Scaffold(
+//                         appBar: AppBar(),
+//                         body: Wrap(
+//                           children: [
+//                             ListTile(
+//                               leading: Icon(
+//                                 Icons.home,
+//                                 color: Colors.white,
+//                               ),
+//                               title: Text(
+//                                 'Header',
+//                                 style: theme.textTheme.subtitle1.copyWith(
+//                                     color: theme.colorScheme.onPrimary),
+//                               ),
+//                               tileColor: theme.colorScheme.primary,
+//                             ),
+//                             ListTile(
+//                               title: Text('Title 1'),
+//                             ),
+//                             ListTile(
+//                               title: Text('Title 2'),
+//                             ),
+//                             ListTile(
+//                               title: Text('Title 3'),
+//                             ),
+//                             ListTile(
+//                               title: Text('Title 4'),
+//                             ),
+//                             ListTile(
+//                               title: Text('Title 5'),
+//                             ),
+//                           ],
+//                         ),
+//                       );
+//                     },
+//                   );
+//                 },
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
